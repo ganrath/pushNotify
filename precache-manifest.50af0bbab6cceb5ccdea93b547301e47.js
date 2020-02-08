62f8670847d15d73cee8439ae9501f5e4d52b467
Bharath Ganesan <ganrath07@gmail.com>
2020-02-08 22:46:08 +0530

    Updates

---
+++ precache-manifest.50af0bbab6cceb5ccdea93b547301e47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31be66b1a833337f8770e08217406129",
+    "url": "/pushNotify/index.html"
+  },
+  {
+    "revision": "216e57b79b5f4ef1fc13",
+    "url": "/pushNotify/static/css/main.25cfca2d.chunk.css"
+  },
+  {
+    "revision": "a52e609ab688bb2c32e8",
+    "url": "/pushNotify/static/js/2.27b4f647.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pushNotify/static/js/2.27b4f647.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "216e57b79b5f4ef1fc13",
+    "url": "/pushNotify/static/js/main.a419eb1e.chunk.js"
+  },
+  {
+    "revision": "addbbf95f2cdf77754bb",
+    "url": "/pushNotify/static/js/runtime-main.3ac9129a.js"
+  }
+]);

6f89e11008fe34ac214bad8a2940e3f0b5e9f6d1
Bharath Ganesan <ganrath07@gmail.com>
2020-02-09 10:40:47 +0530

    service worker

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -138,14 +138,14 @@ export function unregister() {
   }
 }
 
-function askPermission() {
+async function askPermission() {
   return new Promise(function(resolve, reject) {
-    const permissionResult = Notification.requestPermission(function(result) {
+   await const permissionResult = Notification.requestPermission(function(result) {
       resolve(result);
     });
 
     if (permissionResult) {
-      permissionResult.then(resolve, reject);
+      await permissionResult.then(resolve, reject);
     }
   }).then(function(permissionResult) {
     if (permissionResult !== "granted") {
